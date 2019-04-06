0907495d10f591b46101c5d76a48126207f3d2ca
gilescarlos32 <46952491+gilescarlos32@users.noreply.github.com>
2019-04-05 23:50:02 -0700

    Add files via upload

---
+++ heathrowToLondon.hs
@@ -0,0 +1,58 @@
+-- first we see what the best path to the next crossroads on main road A is.
+-- The two options are going directly forward or starting at the opposite road,
+-- going forward and then crossing over. We remember the cost and the path. We use
+-- the same method to see what the best path to the next crossroads on main road B
+-- is and remember that. Then, we see if the path to the next crossroads on A is cheaper
+-- if we go from the previous A crossroads or if we go from the previous B crossroads and
+-- then cross over. We remember the cheaper path and then we do the same for the crossroads
+-- opposite of it. We do this for every section until we reach the end. Once we've reached
+-- the end, the cheapest of the two paths that we have is our optimal path!
+
+import Data.List
+
+data Section = Section { getA :: Int, getB :: Int, getC :: Int } deriving (Show)
+type RoadSystem = [Section]
+
+heathrowToLondon :: RoadSystem
+heathrowToLondon = [Section 50 10 30, Section 5 90 20, Section 40 2 25, Section 10 8 0]
+
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+    let priceA = sum $ map snd pathA
+        priceB = sum $ map snd pathB
+        forwardPriceToA = priceA + a
+        crossPriceToA = priceB + b + c
+        forwardPriceToB = priceB + b
+        crossPriceToB = priceA + a + c
+        newPathToA = if forwardPriceToA <= crossPriceToA
+                        then (A,a):pathA
+                        else (C,c):(B,b):pathB
+        newPathToB = if forwardPriceToB <= crossPriceToB
+                        then (B,b):pathB
+                        else (C,c):(A,a):pathA
+    in  (newPathToA, newPathToB)
+
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+    let (bestAPath, bestBPath) = foldl roadStep ([],[]) roadSystem
+    in  if sum (map snd bestAPath) <= sum (map snd bestBPath)
+        then reverse bestAPath
+        else reverse bestBPath
+
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _ = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
+
+main = do
+    contents <- getContents
+    let threes = groupsOf 3 (map read $ lines contents)
+        roadSystem = map (\[a,b,c] -> Section a b c) threes
+        path = optimalPath roadSystem
+        pathString = concat $ map (show . fst) path
+        pathPrice = sum $ map snd path
+    putStrLn $ "The best path to take is: " ++ pathString
+    putStrLn $ "The price is: " ++ show pathPrice
